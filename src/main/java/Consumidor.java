c049628642ec99f8d5d9960b7a8431d98c7fcc85
Larissa Gusmao <larissamgusmao@gmail.com>
2020-10-22 21:44:21 -0300

    changes

--- src/main/java/Consumidor.java
+++ src/main/java/Consumidor.java
@@ -15,15 +15,36 @@ public class Consumidor {
                 + "";
         canal.queueDeclare(NOME_FILA, false, false, false, null);
 
+        canal.basicQos(1);
+
         DeliverCallback callback = (consumerTag, delivery) -> {
             String mensagem = new String(delivery.getBody());
-            System.out.println("Eu " + consumerTag + " Recebi: " + mensagem);
+            System.out.println("Eu, Larissa de Sousa Moreira Gusmão, recebi: " + mensagem);
+            try {
+                doWork(mensagem);
+            } finally {
+                System.out.println("Done");
+                canal.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
         };
 
         // fila, noAck, callback, callback em caso de cancelamento (por exemplo, a fila foi deletada)
         canal.basicConsume(NOME_FILA, true, callback, consumerTag -> {
             System.out.println("Cancelaram a fila: " + NOME_FILA);
         });
+
+
+    }
+    private static void doWork(String msg) {
+        for(char ch: msg.toCharArray()) {
+            if(ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch(InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 }
 
